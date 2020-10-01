7af1727c7c491d84ce4f27ef61d9d634f3ece651
John Sukup <jesukup@gmail.com>
2020-09-30 18:25:02 -1000

    Web scraper additions

--- scrape_poe.py
+++ scrape_poe.py
@@ -5,13 +5,39 @@ Webscraper for downloading the poems of Edgar Allan Poe for text generation
 using GPT-2-simple from minimaxir's repo 
 (https://github.com/minimaxir/gpt-2-simple).
 
-General format of API requests from poetrydb 
-(https://github.com/thundercomb/poetrydb):
+Scraper uses Beautiful Soup for extracting texts from:
 
-/<input field>/<search term>[;<search term>][..][:<search type>]
-[/<output field>][,<output field>][..][.<format>]
 """
 
 import requests
+import re
+from bs4 import BeautifulSoup
 
-response = requests.get('https://poetrydb.org/author/poe/author')
+base_url ='http://www.poetryloverspage.com/poets/poe/poe_ind.html'
+req = requests.get(base_url)
+soup = BeautifulSoup(req.content, 'html.parser')
+
+poem_urls = soup.select("#display_poem_main a[href$='.html']")
+
+href = []
+for url in poem_urls:
+    href.append(url.get('href'))
+    
+del href[-1] # Last URL not needed in list    
+
+poems = []    
+for url in href:
+ 
+ 
+ 
+ 
+ 
+    # url = 'https://poestories.com{}'.format(url)
+    # req = requests.get(url)
+    # soup = BeautifulSoup(req.content, 'html.parser')
+    # poems.append(soup.find('p', class_=lambda x: 'poem' in x or 'p2' in x).text.strip())
+        
+
+    
+    
+
