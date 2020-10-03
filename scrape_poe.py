1ff7b6f032c2b407c647d2c6b1e450074bbf597e
John Sukup <jesukup@gmail.com>
2020-10-03 11:17:49 -1000

    Completed webscraper for Poe poems. Write to CSV

--- scrape_poe.py
+++ scrape_poe.py
@@ -5,39 +5,45 @@ Webscraper for downloading the poems of Edgar Allan Poe for text generation
 using GPT-2-simple from minimaxir's repo 
 (https://github.com/minimaxir/gpt-2-simple).
 
-Scraper uses Beautiful Soup for extracting texts from:
+Scraper uses Beautiful Soup for extracting poems from:
+http://www.poetryloverspage.com/poets/poe/poe_ind.html
 
 """
 
 import requests
 import re
 from bs4 import BeautifulSoup
+import csv
 
 base_url ='http://www.poetryloverspage.com/poets/poe/poe_ind.html'
 req = requests.get(base_url)
 soup = BeautifulSoup(req.content, 'html.parser')
 
+# Extract tags for each poem URL
 poem_urls = soup.select("#display_poem_main a[href$='.html']")
 
+# Keep only href
 href = []
 for url in poem_urls:
     href.append(url.get('href'))
     
 del href[-1] # Last URL not needed in list    
 
+# Scrape each poem from each href
 poems = []    
 for url in href:
- 
- 
- 
- 
- 
-    # url = 'https://poestories.com{}'.format(url)
-    # req = requests.get(url)
-    # soup = BeautifulSoup(req.content, 'html.parser')
-    # poems.append(soup.find('p', class_=lambda x: 'poem' in x or 'p2' in x).text.strip())
-        
+    url = 'http://www.poetryloverspage.com/poets/poe/{}'.format(url)
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    poem = soup.select_one('#display_poem_main pre')
+    poems.append(poem.text
+                 .strip()
+                 .replace('\n','')
+                 .replace('\\','')
+                 .replace('-',''))
 
-    
-    
-
+# Save to columnar CSV file    
+with open('poe.csv', 'w', newline='') as f:
+    file = csv.writer(f)
+    for poem in poems:
+         file.writerow([poem])
